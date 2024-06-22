ebb88c9cc51674cb2d149115670bbd972763d043
i-m-anurag <iamanurag74@gmail.com>
2024-06-22 21:32:26 +0530

    New changes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const path = require('path');
 const cors = require('cors');
 var bodyParser = require('body-parser')
 require('dotenv').config();
+import { rateLimit } from 'express-rate-limit'
 const category = require('./db/category.json');
 const business = require('./db/business.json');
 const allCategory = require('./db/category_master.json');
@@ -10,6 +11,16 @@ const business_data = require('./db/business_data.json');
 const axios = require('axios')
 
 const app = express();
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+})
+
+// Apply the rate limiting middleware to all requests.
+app.use(limiter)
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
