feaca0290ab6ab31e39aefe501d8eaf84b15c59a
i-m-anurag <iamanurag74@gmail.com>
2025-05-06 23:04:34 +0530

    New Code change added

--- cron/generateBusiness.js
+++ cron/generateBusiness.js
@@ -1,7 +1,7 @@
 const cron = require('node-cron');
 const { generateBusinessData } = require('../service/business.service');
 
-cron.schedule('0 * * * *', async () => { // Every hour
+cron.schedule('0 1 * * *', async () => { // Every day 1 AM
     console.log('[CRON] Running scheduled business data generation...');
     if (process.env.CRON) {
         await generateBusinessData();
