eba822e3e28a7b5e7cd768c9986c8e8affac8339
i-m-anurag <iamanurag74@gmail.com>
2025-05-06 22:49:24 +0530

    CRON Config setup

--- cron/generateBusiness.js
+++ cron/generateBusiness.js
@@ -3,5 +3,8 @@ const { generateBusinessData } = require('../service/business.service');
 
 cron.schedule('0 * * * *', async () => { // Every hour
     console.log('[CRON] Running scheduled business data generation...');
-    await generateBusinessData();
+    if (process.env.CRON) {
+        await generateBusinessData();
+    }
+
 });
